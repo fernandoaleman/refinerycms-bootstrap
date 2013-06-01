7ed63f6561aaa0e88c192a099c811de271af3b5a
Fernando Aleman <fernandoaleman@mac.com>
2013-06-01 09:23:57 -0500

    Initial commit

---
+++ lib/refinerycms/bootstrap.rb
@@ -0,0 +1,7 @@
+require "refinerycms/bootstrap/version"
+
+module Refinerycms
+  module Bootstrap
+    # Your code goes here...
+  end
+end
---
+++ lib/refinerycms/bootstrap/version.rb
@@ -0,0 +1,5 @@
+module Refinerycms
+  module Bootstrap
+    VERSION = "0.0.0"
+  end
+end
